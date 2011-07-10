4cc566c97cb3544e7026ed7ca4bc5b9afe47a095
Jens Maier <jens.maier@uni-ulm.de>
2011-07-11 00:25:37 +0200

    removed template from build

--- MusicBeeAPI/Properties/AssemblyInfo.cs
+++ MusicBeeAPI/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plugin Template")]
-[assembly: AssemblyDescription("A template for MusicBee plugins.")]
+[assembly: AssemblyTitle("MusicBeeAPI")]
+[assembly: AssemblyDescription("An API wrapper for MusicBee")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Example Inc.")]
-[assembly: AssemblyProduct("Plugin Template")]
-[assembly: AssemblyCopyright("Copyright © Example Inc.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicBeeAPI")]
+[assembly: AssemblyCopyright("Copyright © Jens Maier 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
